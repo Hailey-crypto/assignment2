46d5371abe35eb3280b5c3c6c8f9e6172e734c7d
hailey <ahhh0325@gmail.com>
2025-10-02 17:15:59 +0900

    Feat: 비동기 처리로 시간 간격 구현

--- bin/score_advanced.dart
+++ bin/score_advanced.dart
@@ -27,7 +27,7 @@ List<String> names = [];
 List<int> scores = [];
 List<StudentScore> objects = [];
 
-void main() {
+Future<void> main() async {
   // 2. 파일로부터 데이터 읽어오기 기능 ⚪️
   File students = File('students.txt');
   List<String> lines = students.readAsLinesSync();
@@ -54,7 +54,10 @@ void main() {
 
       // 4. 프로그램 종료 후, 결과를 파일에 저장하는 기능 ⚪️
       try {
-        found.saveInfo('result.txt');
+        print('저장 중..');
+        await Future.delayed(Duration(seconds: 2), () {
+          found.saveInfo('result.txt');
+        });
         print('저장이 완료되었습니다.');
       } catch (e) {
         print("저장에 실패했습니다: $e");
@@ -68,11 +71,13 @@ void main() {
 
   // 도전 문제
   while (true) {
-    print(
-      '\n > 메뉴를 선택하세요 : \n 1. 우수생 출력 \n 2. 전체 평균 점수 출력 \n 3. 전체 등수 출력 \n 4. 종료',
-    );
-    String? menu = stdin.readLineSync();
+    await Future.delayed(Duration(seconds: 2), () {
+      print(
+        '\n > 메뉴를 선택하세요 : \n 1. 우수생 출력 \n 2. 전체 평균 점수 출력 \n 3. 전체 등수 출력 \n 4. 종료',
+      );
+    });
 
+    String? menu = stdin.readLineSync();
     switch (menu) {
       case '1':
         // 5-1. 학생들 중 우수생을 출력하는 기능 ⚪️
