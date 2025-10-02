51ac55ba48c39cfd651406c6645789c8029176bc
hailey <ahhh0325@gmail.com>
2025-10-02 17:35:35 +0900

    Refactor: 코드 bin/lib 폴더에 분리 및 파일 이름 수정

--- bin/assignment2.dart
+++ bin/assignment2.dart
@@ -1,27 +1,5 @@
 import 'dart:io';
-
-// 1. Score와 StudentScore 클래스를 구성하기 (객체 지향 활용) ⚪️
-class Score {
-  int score;
-
-  Score(this.score);
-
-  void showInfo() => print('점수: $score');
-}
-
-class StudentScore extends Score {
-  String name;
-
-  StudentScore(this.name, super.score);
-
-  @override
-  void showInfo() => print('이름: $name, 점수: $score');
-
-  void saveInfo(String path) {
-    File result = File(path);
-    result.writeAsStringSync('이름: $name, 점수: $score');
-  }
-}
+import 'package:assignment2/class.dart';
 
 List<String> names = [];
 List<int> scores = [];
